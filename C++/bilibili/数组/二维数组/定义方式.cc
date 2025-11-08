02e3f0656d8fc47bbb6c0f48f6df2193213abdbf
YYWD <qingbin.zhou@estudiantat.upc.edu>
2025-11-08 13:46:29 +0100

    1

--- C++/bilibili/数组/二维数组/定义方式.cc
+++ C++/bilibili/数组/二维数组/定义方式.cc
@@ -2,6 +2,7 @@
 using namespace std;
 
 int main(){
+    
     // 定义方式1
     int arr[2][3]; // 两行三列
     arr[0][0] = 1; arr[0][1] = 2; arr[0][2] = 3; 
@@ -18,4 +19,28 @@ int main(){
         {1,2,3},
         {4,5,6}
     };
+    for(int i=0; i<=1; i++){   // 打印
+        for(int j=0; j<=2; j++){
+            cout << arr2[i][j] << " ";
+        }
+        cout << endl;
+    } 
+
+    // 定义方式3
+    int arr3[2][3] = {1,2,3,4,5,6};
+       for(int i=0; i<=1; i++){   // 打印
+        for(int j=0; j<=2; j++){
+            cout << arr3[i][j] << " ";
+        }
+        cout << endl;
+    } 
+
+    // 定义方式4
+    int arr4[][3] = {1,2,3,4,5,6};
+     for(int i=0; i<=1; i++){   // 打印
+        for(int j=0; j<=2; j++){
+            cout << arr3[i][j] << " ";
+        }
+        cout << endl;
+    }  
 }
